b01efc7c08732b9b7127ce76c3236a33f74a7e01
SK Ali Arman <arman@appscode.com>
2023-11-30 18:34:49 +0600

    working
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- apis/v1beta1/register.go
+++ apis/v1beta1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "azure.azure.kubedb.com"
+	Group   = "azure.kubedb.com"
 	Version = "v1beta1"
 )
 
--- config/cache/config.go
+++ config/cache/config.go
@@ -6,22 +6,10 @@ package cache
 
 import (
 	"github.com/crossplane/upjet/pkg/config"
-
-	"github.com/sheikh-arman/provider-azure/apis/rconfig"
 )
 
 // Configure configures redis group
 func Configure(p *config.Provider) {
-	p.AddResourceConfigurator("azurerm_redis_linked_server", func(r *config.Resource) {
-		r.References["linked_redis_cache_id"] = config.Reference{
-			Type:      "RedisCache",
-			Extractor: rconfig.ExtractResourceIDFuncPath,
-		}
-		r.References["target_redis_cache_name"] = config.Reference{
-			Type: "RedisCache",
-		}
-		delete(r.References, "linked_redis_cache_location")
-	})
 
 	p.AddResourceConfigurator("azurerm_redis_cache", func(r *config.Resource) {
 		config.MarkAsRequired(r.TerraformResource, "redis_version")
